87a40195b69b700e47bd3e476a96baf06c9ebd8f
pochita0109 <akosicosta@gmail.com>
2023-04-19 15:31:04 -0700

    first commit

---
+++ 2ndproblem.py
@@ -0,0 +1,13 @@
+# Kenneth John Costa
+# Assignment #2
+# Decryption
+
+# Ask the user to enter the message to decrypt
+# Substitute the characters by replacing it to corresponding vowels
+# If the character is *, change to a
+# If the character is &, change to e
+# If the character is #, change to i
+# If the character is +, change to o
+# If the character is !, change to u
+# Print the decrypted message
+# Ask the user if he/she wants to decrypt again
